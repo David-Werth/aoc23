22e97cb089134a0d9334f0ef5a7126ec541b6549
David-Werth <75678655+David-Werth@users.noreply.github.com>
2023-12-11 17:50:10 +0100

    solution: day 3 part 2

--- dayThree/dayThree.js
+++ dayThree/dayThree.js
@@ -31,26 +31,26 @@ const partOne = (input) => {
 		for (let j = 0; j < cols; j++) {
 			let char = input[i][j];
 			let directions = [
-				[[i - 1], [j - 1]],
-				[[i - 1], [j]],
-				[[i - 1], [j + 1]],
-				[[i], [j - 1]],
-				[[i], [j + 1]],
-				[[i + 1], [j - 1]],
-				[[i + 1], [j]],
-				[[i + 1], [j + 1]],
+				[i - 1, j - 1],
+				[i - 1, j],
+				[i - 1, j + 1],
+				[i, j - 1],
+				[i, j + 1],
+				[i + 1, j - 1],
+				[i + 1, j],
+				[i + 1, j + 1],
 			];
 
 			if (isNumber(char)) {
 				currentNumber += char;
 				for (let k = 0; k < directions.length; k++) {
 					if (
-						input[directions[k][0][0]] === undefined ||
-						input[directions[k][0][0]][directions[k][1][0]] === undefined
+						input[directions[k][0]] === undefined ||
+						input[directions[k][0]][directions[k][1]] === undefined
 					)
 						continue;
 
-					let currentCheckValue = input[directions[k][0][0]][directions[k][1][0]];
+					let currentCheckValue = input[directions[k][0]][directions[k][1]];
 
 					if (!isDot(currentCheckValue) && !isNumber(currentCheckValue)) {
 						hasSymbolNeigbour = true;
@@ -66,11 +66,99 @@ const partOne = (input) => {
 		}
 	}
 
-	console.log(sum);
+	return sum;
 };
 
-partOne(input);
+const partTwo = (input) => {
+	const cols = input[0].length;
+	const rows = input.length;
 
-// const partTwo = (input) => {};
+	let total = 1;
+	let allNumbers = {};
+	let gearRatioValues = [];
 
-// console.log(`Part One: ${partOne(input)}, Part Two: ${partTwo(input)}`);
+	const isNumber = (char) => {
+		return !isNaN(parseInt(char));
+	};
+
+	const isDot = (char) => {
+		return char === '.';
+	};
+
+	const isAsterisk = (char) => {
+		return char === '*';
+	};
+
+	let currentNumber = '';
+	let hasAsteriskNeighbour = false;
+	let checkValueLocation;
+
+	for (let i = 0; i < rows; i++) {
+		if (currentNumber && hasAsteriskNeighbour)
+			allNumbers[checkValueLocation] = allNumbers[checkValueLocation]
+				? [...allNumbers[checkValueLocation], parseInt(currentNumber)]
+				: [parseInt(currentNumber)];
+		currentNumber = '';
+		hasAsteriskNeighbour = false;
+
+		for (let j = 0; j < cols; j++) {
+			let char = input[i][j];
+			let directions = [
+				[i - 1, j - 1],
+				[i - 1, j],
+				[i - 1, j + 1],
+				[i, j - 1],
+				[i, j + 1],
+				[i + 1, j - 1],
+				[i + 1, j],
+				[i + 1, j + 1],
+			];
+
+			if (isNumber(char)) {
+				currentNumber += char;
+				for (let k = 0; k < directions.length; k++) {
+					if (
+						input[directions[k][0]] === undefined ||
+						input[directions[k][0]][directions[k][1]] === undefined
+					)
+						continue;
+
+					let checkValue = input[directions[k][0]][directions[k][1]];
+
+					if (isAsterisk(checkValue)) {
+						checkValueLocation = `${directions[k][0]}-${directions[k][1]}`;
+						hasAsteriskNeighbour = true;
+
+						allNumbers[checkValueLocation] = allNumbers[checkValueLocation]
+							? allNumbers[checkValueLocation]
+							: [];
+					}
+				}
+			} else {
+				if (currentNumber && hasAsteriskNeighbour) {
+					allNumbers[checkValueLocation] = allNumbers[checkValueLocation]
+						? [...allNumbers[checkValueLocation], parseInt(currentNumber)]
+						: [parseInt(currentNumber)];
+				}
+				currentNumber = '';
+				hasAsteriskNeighbour = false;
+			}
+		}
+	}
+
+	for (const key in allNumbers) {
+		if (allNumbers[key].length === 2) {
+			gearRatioValues = [
+				...gearRatioValues,
+				allNumbers[key].reduce((a, b) => a * b),
+			];
+		}
+	}
+
+	total = gearRatioValues.reduce((a, b) => a + b);
+
+	return total;
+};
+
+// console.log(partTwo(exampleOne));
+console.log(`Part One: ${partOne(input)}, Part Two: ${partTwo(input)}`);
